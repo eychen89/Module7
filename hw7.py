5cfc572fccb1729e0b11a33c206fd234a74f08c7
Eric Chen <eric.chen.y@gmail.com>
2017-08-16 16:35:29 -0700

    Completed homework 7

--- hw7.py
+++ hw7.py
@@ -1,8 +1,9 @@
 from sortedcontainers import SortedDict
 
-#
-# Adding a comment here
-#
+"""
+This script was modified for Homework 7.
+The script works with a dictionary to find and modify values.
+"""
 
 def print_menu():
     print('1. Print Users')
@@ -32,35 +33,43 @@ while menu_choice != 5:
     # get menu choice from user
     menu_choice = int(input("Type in a number (1-5): "))
 
-    # view current entries
+    # View the current users contained in the dictionary
     if menu_choice == 1:
         print("Current Users:")
         for x, y in usernames.items():
             print("Name: {} \tUser Name: {} \n".format(x, y))
 
-    # add an entry
+    # Add a user to the dictionary
     elif menu_choice == 2:
         print("Add User")
         name = input("Name: ")
         username = input("User Name: ")
         usernames[name] = username
+        if not name or not username:   # Handles exception for empty name or username
+            print("Please enter a valid name.")
+            name = input("Name: ")
+            username = input("User Name: ")
+            usernames[name] = username
 
-    # remove an entry
+    # Remove a user from the dictionary
     elif menu_choice == 3:
         print("Remove User")
         name = input("Name: ")
         if name in usernames:
-            pass  # delete that entry
+            usernames.pop(name)  # delete that entry
+        else:
+            print("Please try again with a valid name.")  # Handles exceptions
 
-    # view user name
+    # Lookup user
     elif menu_choice == 4:
         print("Lookup User")
         name = input("Name: ")
         if name in usernames:
-            pass  # print the username
+            print(f'{name} was found in the dictionary')  # print the username
         else:
-            pass  # print username not found
+            print(f'{name} was not found in the dictionary') # print username not found
 
-    # is user enters something strange, show them the menu
+    # If the user enters a value other than 1-5, display the main menu again.
     elif menu_choice != 5:
+        print("Please enter a valid option.")  # Prompts the user to enter a valid option to try again
         print_menu()
